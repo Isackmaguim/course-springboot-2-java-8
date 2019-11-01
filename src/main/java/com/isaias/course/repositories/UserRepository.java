9ae11fdae13d44396308531b43ebc6c01cd5b696
Isackmaguim <isackmaguim@hotmail.com>
2019-11-01 18:55:35 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/isaias/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.isaias.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.isaias.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
